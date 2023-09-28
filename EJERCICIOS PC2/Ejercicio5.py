eb674cb954066f9f335e4da6d3a62ee3527d3612
Bertha31 <145274441+Bertha31@users.noreply.github.com>
2023-09-28 01:15:42 +0000

    uj

---
+++ EJERCICIOS PC2/Ejercicio5.py
@@ -0,0 +1,14 @@
+def validacion(numero, digito):
+    numero_str=str(numero)
+    cantidad=numero_str.count(str(digito))
+    return cantidad
+while True:
+    try:
+        numero1 = int(input('Ingrese el número'))
+        digito1= int(input('Ingrese el digito'))
+        print(f"Número ingresado: {numero1}") 
+        print(f"Digito: {digito1}") 
+        print(f"Cantidad de veces {digito1} en el número: {validacion(numero1,digito1)}")
+        break
+    except ValueError:
+        print('Digitación incorrecta. Ingrese el número de nuevo')
