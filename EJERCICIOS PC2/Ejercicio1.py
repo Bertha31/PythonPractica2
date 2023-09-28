c976422d4d48ce934323890d7497b61971bd9d73
Bertha31 <145274441+Bertha31@users.noreply.github.com>
2023-09-28 01:28:51 +0000

    pc2

--- EJERCICIOS PC2/Ejercicio1.py
+++ EJERCICIOS PC2/Ejercicio1.py
@@ -1,6 +1,6 @@
 numeros = []
 for numero in range(1500, 2701):
-    if numero %5 and numero %7== 0:
+    if numero %5 ==0 and numero %7== 0:
         numeros.append(numero)
 print("Los números divisibles por 7 y múltiplos de 5 en el rango de 1500 a 2700 son:")
 print(numeros)
