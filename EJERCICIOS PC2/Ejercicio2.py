f49564b42c780258e7054b24ca5466fd2bd45a69
Bertha31 <145274441+Bertha31@users.noreply.github.com>
2023-09-28 01:07:48 +0000

    ff

--- EJERCICIOS PC2/Ejercicio2.py
+++ EJERCICIOS PC2/Ejercicio2.py
@@ -1,10 +1,14 @@
-h= int(input('Ingrese la altura del triangulo: '))
-while h<0:
-    print('Número incorrecto')
-    h=int(input('Vuelva a ingressar el numero: '))
-    pass
-else:
-    for i in range(h):
-        print('*'*(i+1))
-    for i in range(h,0,-1):
-        print('*'*(i))
+while True:
+    try:
+        h= int(input('Ingrese la altura del triangulo: '))
+        while h<0:
+            h=int(input('Número negativo. Vuelva a ingresar el numero: '))
+            pass
+        else:
+            for i in range(h):
+                print('*'*(i+1))
+            for i in range(h,0,-1):
+                print('*'*(i))
+        break
+    except ValueError:
+        print('Número incorrecto. Por favor, ingrese un número entero no negativo.')
--- EJERCICIOS PC2/Ejercicio3.py
+++ EJERCICIOS PC2/Ejercicio3.py
@@ -2,22 +2,22 @@ numeros_total= []
 pares =0
 impares=0
 while True:
-    numero1= input('Desea ingresar un número?: ')
-    numero1= numero1.lower()
-    while numero1 != 'si' and numero1 != 'no':
-        numero1=input('Digitación incorrecta. Vuelva a ingresar: ')
-    if numero1 == 'si': 
+    ingreso= input('¿Desea ingresar un número? (SI/NO): ')
+    ingreso= ingreso.lower()
+    while ingreso != 'si' and ingreso != 'no':
+        ingreso=input('Digitación incorrecta. Vuelva a ingresar: ')
+    if ingreso == 'si': 
         try:
-            numero2= float(input('Ingrese número: '))
-            numeros_total.append(numero2)
-            if numero2 %2 == 0:
+            numero= float(input('Ingrese número: '))
+            numeros_total.append(numero)
+            if numero %2 == 0:
                 pares += 1
             else:
                 impares += 1
 
         except ValueError:
             print("Por favor, ingrese un número válido.")
-    if numero1 == 'no':
+    if ingreso == 'no':
         break
 print(f"Números ingresados: {numeros_total}")
 print(f"La cantidad de numeros pares son: {pares}")
